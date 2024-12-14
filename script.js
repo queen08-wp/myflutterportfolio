2626e4e3fa50445c57fe96bec6f9df429425c048
queen08-wp <dalumpinesqueenie08@gmail.com>
2024-12-14 12:09:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Display a Welcome Alert When the Page Loads
+//window.addEventListener('load', function () {
+ //   alert('Welcome to My Portfolio!');
+});
+
+// Smooth Scroll for Navigation Links
+//const navLinks = document.querySelectorAll('nav a');
+//navLinks.forEach(link => {
+ //   link.addEventListener('click', function (event) {
+ //       event.preventDefault(); // Prevent default anchor behavior
+ //       const targetId = this.getAttribute('href').substring(1); // Get the target section ID
+//        const targetSection = document.getElementById(targetId); //
+
+        // Smooth scroll to the target section
+ //       targetSection.scrollIntoView({
+//            behavior: 'smooth'
+  //      });
+ //   });
+//});
+
+// Show a Message When a Project Is Clicked
+//projectTitles.forEach(title => {
+//    title.addEventListener('click', function () {
+ //       alert(`You clicked on: ${this.textContent}`);
+//});
